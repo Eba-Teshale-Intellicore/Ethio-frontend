621013f6ab13494108b10961d8cdd7e4c6bba512
Eba Teshale <ebateshale40@gmail.com>
2026-04-29 10:18:38 +0300

    sign up

---
+++ app/signup/page.tsx
@@ -0,0 +1,14 @@
+
+import Signup from "@/components/auth/Signup"
+import Footer from "@/components/Footer";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <div>
+        <Signup/>
+        {/* <Footer/> */}
+      </div>
+    </main>
+  );
+}
