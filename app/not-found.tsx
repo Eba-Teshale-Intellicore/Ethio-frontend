5b143e57374dea25d878acec7ab911a915b26e82
Eba Teshale <ebateshale40@gmail.com>
2026-05-11 11:22:40 +0300

    notfound page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,7 +3,7 @@ import styles from "./custom.module.scss";
 
 export default function NotFound() {
   return (
-    <div className={styles.fcontainer}>
+    <div className={styles.container}>
       <h1 className={styles.title}>404 - Page Not Found</h1>
       <p className={styles.message}>
         Oops! The page you're looking for doesn't exist.
