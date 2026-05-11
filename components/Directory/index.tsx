92cf5b25819a576ca0e243479593a33212542166
Eba Teshale <ebateshale40@gmail.com>
2026-05-11 11:29:48 +0300

    to 8

--- components/Directory/index.tsx
+++ components/Directory/index.tsx
@@ -585,7 +585,7 @@ export default function Directory() {
             </motion.button>
           )}
 
-          {visibleCount > 6 && (
+          {visibleCount > 8 && (
             <motion.button
               className={styles.loadMoreBtn}
               whileHover={{ scale: 1.04 }}
