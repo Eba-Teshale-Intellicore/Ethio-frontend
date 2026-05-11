46027ce78da1a00d23435e25e719c3d1dbeb699f
Eba Teshale <ebateshale40@gmail.com>
2026-05-11 09:58:37 +0300

    twenty

--- components/Directory/index.tsx
+++ components/Directory/index.tsx
@@ -486,7 +486,7 @@ export default function Directory() {
   const [heroes, setHeroes] = useState<Heroes[]>([]);
   const [eras, setEra] = useState<Era[]>([])
   const [categories, setCategories] = useState<Category[]>([])
-  const [visibleCount, setVisibleCount] = useState(12);
+  const [visibleCount, setVisibleCount] = useState(20);
   const [selectedEra, setSelectedEra] = useState("");
   const [selectedCategory, setSelectedCategory] = useState("");
 
